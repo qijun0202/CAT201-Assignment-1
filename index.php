52a451896594d7c02992ac80710259cd630d1fd8
Lim Qi Jun <limqijun@gmail.com>
2021-12-12 02:59:28 +0800

    add index.php for testing

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>PDF to text conversion</title>
+    </head>
+    
+    <body>
+        <div class="web_title">
+            <h1>Welcome! PDF to text conversion</h1>
+        </div>
+        
+    </body>
+</html>
