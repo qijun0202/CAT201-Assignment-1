be4cd57734079a3c4da5943f2e5e9aac1487952e
Lim Qi Jun <limqijun@gmail.com>
2021-12-13 02:12:22 +0800

    test PDF file upload function

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title>PDF to text conversion</title>
     </head>
     
     <body>
-        <div class="web_title">
-            <h1>Welcome! PDF to text conversion</h1>
+        <section class="header">
+            <div class="web_title">
+                <h1>Welcome! PDF to text conversion</h1>
+            </div>
+        </section>
+
+        <div class="file-upload">
+            <h2>Please upload PDF file for conversion.</h2>
+            <form action="file-upload.php" method="post" enctype="multipart/form-data">
+                Select PDF file to upload:<br>
+                <input type="file" name="pdfFileToUpload" id="pdfFileToUpload">
+                <input type="submit" value="Upload PDF File" name="submit">
+            </form>
         </div>
-        
+            
+        <section class="footer">
+            <h5>Disclaimer: This page is created for assignment purpose only and any </h5>
+        </section>
     </body>
 </html>
